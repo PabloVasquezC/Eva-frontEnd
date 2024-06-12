decfbe39bd9e0a4363b0b236695e0ad3819bb02e
Laboratorio Computacion Talca <tal_lab@inacap.cl>
2024-06-11 20:02:20 -0400

    firstCommit

---
+++ Eva 3 - JS/js/index.js
@@ -0,0 +1,16 @@
+const nombre = document.getElementById('nombre');
+const apellido = document.getElementById('apellido');
+const numero1 = document.getElementById('numero1');
+const numero2 = document.getElementById('numero2');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+const dosNumeros = () => {
+    var value1 = numero1.value 
+    var value2 = numero2.value 
+
+    if ( value1 != "" && value2 != "") {
+        
+    }
+}
+
